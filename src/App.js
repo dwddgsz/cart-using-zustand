c126b7444782bd09de3171c9d2bcb4cd2e69b08e
dawiddlugosz <dawid.dlugosz@ekookna.pl>
2021-09-21 07:43:16 +0200

    init: clean cra, install packages, global styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components"; 
+import styled from "styled-components";
 
-function App() {
+const globalStyle = createGlobalStyle`
+
+:root {
+  --light:#fff;
+  --subtitle: #333;
+  --dark:#000;
+}
+
+*,
+*::before,
+**after {
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+html,
+body {
+  height:100%;
+}
+
+ol,
+ul {
+  list-style:none;
+}
+
+a {
+  text-decoration: none;
+  color:var(--dark);
+}
+
+img {
+  display:block;
+}
+`
+
+const AppWrapper = styled.div`
+  max-width:1440px;
+  margin:0 auto;
+  font-family: 'Poppins';
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <globalStyle>
+    <AppWrapper>
+      <BrowserRouter>
+        test
+      </BrowserRouter>
+    </AppWrapper>
+  </globalStyle>
+  )
 }
 
 export default App;
